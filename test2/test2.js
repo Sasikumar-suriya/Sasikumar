a5315158d78a79e917317d6fdbc574783034d75b
Sasikumar-suriya <134589283+Sasikumar-suriya@users.noreply.github.com>
2023-06-10 22:06:52 +0530

    Add files via upload

---
+++ test2/test2.js
@@ -0,0 +1,9 @@
+function myfunction(){
+    alert("Your logging into Menu page");
+}
+function myfunction_1(){
+    alert("Your logging into Contact page");
+}
+function myfunction_2(){
+    alert("Order now your favourite foods here");
+}
